2c63ab73896ed5ef9f38354d361768d2ca0ca13d
guoye <guoye@nmcc.ncr>
2017-04-05 15:53:01 +0800

    'push'

--- js/common.js
+++ js/common.js
@@ -3,6 +3,14 @@ import 'polyfill';
 // 基础库
 import Vue from 'vue';
 import $ from 'jquery';
+window.LOG = function () {
+    if (window.console && window.console.log) {
+        var args = Array.prototype.slice.call(arguments);
+        args.forEach(function (v) {
+            window.console.log(v);
+        });
+    }
+};
 export {
 	Vue,
 	$
--- js/entry/index.js
+++ js/entry/index.js
@@ -1,14 +1,23 @@
 import {Vue} from 'common';
-
+import detect from 'util/detect';
 let methods = {};
 methods.alert = function () {
 };
-let mounted = function () {};
+let computed = {};
+computed.className = function () {
+    let env = this.env;
+    let os = 'os-' + (env.os.phone ? 'phone' : (env.os.tablet ? 'tablet' : 'pc'));
+    return [os];
+};
+let mounted = function () {
+};
 window.Index = new Vue({
     el: '#main',
     mounted,
+    computed,
     methods,
     data: {
-        name: 'index-123'
+        name: 'index-123',
+        env: detect()
     }
 });
